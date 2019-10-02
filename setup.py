32f2d265245ffd046bda26b825358b36ebc5b4a2
Miguel Merino <miguel_dreimal_30@hotmail.com>
2019-10-02 13:26:26 +0200

    first release 1.3.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
           'configparser',
           'pywt',
           'scipy',
-          'mysql',
+          'mysql-connector-python',
           'logging'
 
       ],
@@ -34,5 +34,6 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 )
