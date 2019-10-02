9935956417349772c2b60fbd927a70d114bebc3d
Miguel Merino <miguel_dreimal_30@hotmail.com>
2019-10-02 13:41:15 +0200

    first release 1.3.5

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name='edwar',         # How you named your package folder (MyLib)
   packages=['edwar'],   # Chose the same as "name"
-  version='0.2',      # Start with a small number and increase it with every change you make
+  version='0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='signal Error Detection for WBSN And data1 Recovery',   # Give a short description about your library
   author='Miguel Merino',                   # Type in your name
   author_email='miguel_dreimal_30@hotmail.com',      # Type in your E-Mail
   url='https://github.com/greenlsi/edwar',   # Provide either the link to your github or to your website
-  download_url='https://github.com/greenlsi/edwar/archive/v_02.tar.gz',
+  download_url='https://github.com/greenlsi/edwar/archive/v_03.tar.gz',
   keywords=['EDA', 'RECOVERY', 'SIGNAL'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
