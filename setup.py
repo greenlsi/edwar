e25b5854479290f91ce2df9ca20017122d49ac83
Miguel Merino <miguel_dreimal_30@hotmail.com>
2019-10-02 12:44:58 +0200

    first release 1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name='edwar',         # How you named your package folder (MyLib)
   packages=['edwar'],   # Chose the same as "name"
-  version='0.1',      # Start with a small number and increase it with every change you make
+  version='0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='signal Error Detection for WBSN And data1 Recovery',   # Give a short description about your library
   author='Miguel Merino',                   # Type in your name
