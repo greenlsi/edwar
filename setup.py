bccea15cfe3adce005dc1d9e676d99c99fe34c43
josueportiz <josueportiz@gmail.com>
2020-09-30 12:39:45 +0200

    Updating dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+xfrom setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
   name='edwar',         # How you named your package folder (MyLib)
   packages=find_packages(),   # Chose the same as "name"
-  version='0.9.1.1',      # Start with a small number and increase it with every change you make
+  version='0.9.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license
   include_package_data=True,
   description='signal Error Detection for WBSN And data1 Recovery',   # Give a short description about your library
@@ -25,11 +25,11 @@ setup(
           'configparser',
           'pycryptodome',
           'PyWavelets',
-          'scipy',
-          'mysql',
+          'scipy',          
           'mysql-connector-python',
-          'ledapy'
-  ],
+          'ledapy',
+          'cvxopt'      
+  ], # 'mysql' used to be neceessary
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the
                                             # current state of your package
