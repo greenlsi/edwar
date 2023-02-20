196592bd906e3cdaccc807e265d0ee47027df298
martaveronaalmeida <marta.verona-almeida@eesy-innovation.com>
2023-02-20 13:45:04 +0100

    Fix cvxopt package version

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ setup(
           'scipy',          
           'mysql-connector-python',
           'ledapy',
-          'cvxopt', 'joblib'
+          'cvxopt == 1.2.6', 
+          'joblib'
   ], # 'mysql' used to be neceessary
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the
