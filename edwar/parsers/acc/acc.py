e334a722bcac456f650f298c0695c6adc5752719
martaveronaalmeida <marta.verona-almeida@eesy-innovation.com>
2023-02-21 11:46:49 +0100

    Fix missing values error and warning with features names

--- edwar/parsers/acc/acc.py
+++ edwar/parsers/acc/acc.py
@@ -62,9 +62,13 @@ def predict_activity(features):
                                               'models/finalized_raw_model_leftHand.sav'))
     model_act_right = joblib.load(os.path.join(script_dir,
                                                'models/finalized_raw_model_rightHand.sav'))
-    result = model_hand.predict(features)
-    result1 = model_act_left.predict(features)
-    result2 = model_act_right.predict(features)
+    # Fill nan values for model prediction
+    features.fillna(method='bfill', inplace=True)
+
+    # Predict activity
+    result = model_hand.predict(features.values)
+    result1 = model_act_left.predict(features.values)
+    result2 = model_act_right.predict(features.values)
     features['hand'] = ['rightHand' if x < 1.5 else 'leftHand' for x in result]
     features['activity'] = result1
     features['activity2'] = result2
